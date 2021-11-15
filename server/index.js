53094a3a5e757cfbee031fc06fdb4d2b5de573d4
Stan <stanbarkmeijer@hotmail.com>
2021-11-15 16:14:22 +0100

    CD intergration

--- server/index.js
+++ server/index.js
@@ -5,12 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/carpediemshipping-paint'));
+app.use(express.static('./dist/carpediemshipping-paint'));
 
 app.get('/*', function (req, res) {
-
-  res.sendFile(path.join(__dirname + '/dist/carpediemshipping-paint/index.html'));
+  res.sendFile(path.join('/dist/carpediemshipping-paint/index.html'), { root: "./" });
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8081);
