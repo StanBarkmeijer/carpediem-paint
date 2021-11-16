5803650e2eb267beaacb92c0798dff6a8090d2f9
Stan <stanbarkmeijer@hotmail.com>
2021-11-16 16:25:08 +0100

    Delete user added

---
+++ src/app/user/user-detail/user-detail.component.ts
@@ -0,0 +1,52 @@
+import { Location } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { User } from '../user';
+import { UserService } from '../../user.service';
+
+@Component({
+  selector: 'app-user-detail',
+  templateUrl: './user-detail.component.html',
+  styleUrls: ['./user-detail.component.css']
+})
+export class UserDetailComponent implements OnInit {
+
+  @Input() user?: User;
+
+  constructor(
+    private userService: UserService,
+    private route: ActivatedRoute,
+    private location: Location,
+    private toastr: ToastrService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.getUser();
+  }
+
+  getUser(): void {
+    this.route.params.subscribe((param) => {
+      const id = Number(param["id"]);
+
+      this.userService.getUser(id)
+        .subscribe((user) => this.user = user);
+    }) 
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  deleteUser(id: number): void {
+    this.userService.deleteUser(id);
+
+    this.toastr.success(`User with id: ${id} deleted`, "User deleted",  {
+      progressBar: true
+    });
+
+    this.location.back();
+  }
+
+}
---
+++ src/app/user/users/user.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { User } from '../user';
+import { UserService } from '../../user.service';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+
+  users: User[] = [];
+
+  constructor(
+    private userService: UserService,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(() => this.getUsers());
+  }
+
+  getUsers(): void {
+    this.userService.getUsers()
+      .subscribe((users) => this.users = users);
+  }
+
+}
