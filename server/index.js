40cbace8794d5d224738342cc588128d876772ae
Stan <stanbarkmeijer@hotmail.com>
2021-12-08 13:05:01 +0100

    Port change

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const app = require("./config/express");
 require("./config/mongoose");
 
 if (!module.parent) {
-  app.listen(8081, () => {
+  app.listen(process.env.PORT || 8081, () => {
     console.info(`Server started on port ${config.port} (${config.env})`)
   });
 }
